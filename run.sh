b7aab66981ee90507f87714db322b4ceff23c131
Валь Максим <makaleval@edu.hse.ru>
2026-06-08 18:26:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+build_generator() {
+    docker build -t generator .
+}
+
+run_generator() {
+    mkdir -p ./data
+    docker run --rm \
+        -v $(pwd)/data:/app/data \
+        generator \
+        python generate.py "/app/data"
+}
+
+create_local_data() {
+    mkdir -p ./local_data
+    python generate.py "./local_data"
+}
