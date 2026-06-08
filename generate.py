d6878c76860afbc28210e86ab7dc1ad0fcd2ec24
Валь Максим <makaleval@edu.hse.ru>
2026-06-08 14:04:22 +0300

    Update generate.py
    добавил названия колонок

--- generate.py
+++ generate.py
@@ -6,15 +6,15 @@ import sys
 NUM_ROWS = 50
 
 
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["ID", "Кол-во лейкоцитов", "Кол-во тромбоцитов", "Группа крови"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "ID": random.randint(0, 100),
+        "Кол-во лейкоцитов": round(random.uniform(1.5, 9.9), 2),
+        "Кол-во тромбоцитов": random.randint(0, 100),
+        "Группа крови": random.choice(["A", "B", "C"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
